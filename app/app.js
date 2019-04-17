79f4172f4b98c653b353951b601439b1044da465
Anton von Weltzien <a.weltzien@outlook.de>
2019-04-17 15:16:22 +0200

    Readd Semicolons

--- app/app.js
+++ app/app.js
@@ -9,9 +9,9 @@ var modeler = new ChoreoModeler({
     keyboard: {
         bindTo: document
     }
-})
+});
 
-renderModel(xml)
+renderModel(xml);
 
 function renderModel(new_xml) {
     modeler.setXML(new_xml).then(result => {
@@ -37,18 +37,18 @@ function saveDiagram(done) {
 }
 
 $(function () {
-    var downloadLink = $('#js-download-diagram')
-    var downloadSvgLink = $('#js-download-svg')
+    var downloadLink = $('#js-download-diagram');
+    var downloadSvgLink = $('#js-download-svg');
 
     $('.buttons a').click(function (e) {
         if (!$(this).is('.active')) {
-            e.preventDefault()
+            e.preventDefault();
             e.stopPropagation()
         }
-    })
+    });
 
     function setEncoded(link, name, data) {
-        var encodedData = encodeURIComponent(data)
+        var encodedData = encodeURIComponent(data);
         if (data) {
             link.addClass('active').attr({
                 'href': 'data:application/bpmn20-xml;charset=UTF-8,' + encodedData,
@@ -62,39 +62,39 @@ $(function () {
     var exportArtifacts = debounce(function () {
         saveSVG(function (err, svg) {
             setEncoded(downloadSvgLink, 'diagram.svg', err ? null : svg)
-        })
+        });
         saveDiagram(function (err, xml) {
             setEncoded(downloadLink, 'diagram.bpmn', err ? null : xml)
         })
-    }, 500)
+    }, 500);
 
-    modeler.on('commandStack.changed', exportArtifacts)
+    modeler.on('commandStack.changed', exportArtifacts);
     exportArtifacts()
-})
+});
 
 $(function () {
     $('input').change(function (e) {
-        var reader = new FileReader()
-        var file = document.querySelector('input[type=file]').files[0]
+        var reader = new FileReader();
+        var file = document.querySelector('input[type=file]').files[0];
         reader.addEventListener("load", function () {
-            const new_xml = reader.result
+            const new_xml = reader.result;
             renderModel(new_xml)
-        }, false)
+        }, false);
 
         if (file) {
             reader.readAsText(file)
         }
 
     })
-})
+});
 // expose bpmnjs to window for debugging purposes
-window.bpmnjs = modeler
+window.bpmnjs = modeler;
 
 // helpers //////////////////////
 
 function debounce(fn, timeout) {
 
-    var timer
+    var timer;
 
     return function () {
         if (timer) {
