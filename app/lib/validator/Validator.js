ef58e8ebf4e715ebca78ca3f35bd2e4b48210cda
Anton von Weltzien <a.weltzien@outlook.de>
2019-05-03 13:48:02 +0200

    Wip Validator work

--- app/lib/validator/Validator.js
+++ app/lib/validator/Validator.js
@@ -2,7 +2,17 @@ import {
   is
 } from 'bpmn-js/lib/util/ModelUtil';
 
-function getPredecessors(shape) {
+/**
+ * Get connected Choreo Activities
+ * @param shape
+ * @param direction 'incoming' || 'outgoing'
+ * @returns {Array}
+ */
+function getConnected(shape, direction) {
+  if (direction !== 'incoming' && direction !== 'outgoing') {
+    // This would currently reload the page due to debounce perhaps?
+    throw new Error('Illegal Argument: ' + direction);
+  }
   if (is(shape, 'bpmn:Participant')) {
     shape = shape.parent;
   }
@@ -12,31 +22,34 @@ function getPredecessors(shape) {
   }
 
   let visited = [];
-  let predecessors = [];
+  let connected = [];
 
-  function backtrack(nodeShape) {
+  function track(nodeShape, direction) {
+    const flowDirection = direction === 'incoming' ? 'source': 'target';
     // avoid loops
     if (visited.includes(nodeShape)) {
       return;
     }
     visited.push(nodeShape);
 
-    // add to predecessors if we have reached an activity
+    // add to connected if we have reached an activity
     if (shape !== nodeShape && is(nodeShape, 'bpmn:ChoreographyActivity')) {
-      predecessors.push(nodeShape);
+      connected.push(nodeShape);
       return;
     }
 
-    // iterate through all incoming sequence flows and
-    // recurse into the sources
-    nodeShape.incoming.forEach(flow => {
-      backtrack(flow.source);
+    // iterate through all incoming or outgoing sequence flows and
+    // recurse into the sources or targets
+    nodeShape[direction].forEach(flow => {
+      track(flow[flowDirection], direction);
     });
   }
 
-  backtrack(shape);
-  return predecessors;
+  track(shape, direction);
+  return connected;
 }
+
+
 
 function getParticipants(shape) {
   if (is(shape, 'bpmn:Participant')) {
@@ -59,7 +72,7 @@ function isInitiating(shape) {
 
 function simpleFlowConstraint(shape, reporter) {
   if (!shape.hidden && is(shape, 'bpmn:Participant')) {
-    let predecessors = getPredecessors(shape);
+    let predecessors = getConnected(shape, 'incoming');
     console.log(shape, predecessors);
 
     if (isInitiating(shape)) {
@@ -76,8 +89,21 @@ function simpleFlowConstraint(shape, reporter) {
   }
 }
 
+/**
+ *
+ * @param shape
+ * @param reporter {Reporter}
+ */
 function eventBasedGateway(shape, reporter) {
   if (is(shape, 'bpmn:EventBasedGateway')) {
+    const following = getConnected(shape, 'outgoing');
+    // TODO: Using flatmap would require a polyfill for MS-Edge. We should clean up the babel part and take care of it that way
+    const senders = following.flatMap(a => a.bandShapes.filter(p => isInitiating(p)));
+    const receivers = following.flatMap(a => a.bandShapes.filter(p => !isInitiating(p)));
+    if (!(senders.every((s, i,a) => a[0].businessObject.id === s.businessObject.id) ||
+      receivers.every((r,i,a) => a[0].businessObject.id === r.businessObject.id))) {
+      reporter.report(shape,2, 'After an Event Based Gateway all senders or all receivers must be the same');
+    }
     debugger;
   }
 }
@@ -94,7 +120,7 @@ class Reporter {
         top: 0,
         left: 0
       },
-      html: '<div class="diagram-note">❌</div>'
+      html: '<div title="This is my tooltip" class="diagram-note" >❌ </div>'
     });
   }
   clearAll() {
